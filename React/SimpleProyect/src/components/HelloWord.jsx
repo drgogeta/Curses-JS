e2eda639e4e78a71a20cf03afee0432ffbe0d885
Daniel Castellanos <daniel.castellanos@xenial.com>
2020-01-08 14:43:14 -0500

    configure react project

---
+++ React/SimpleProyect/src/components/HelloWord.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import '../../assets/styles/App.scss'
+
+const HelloWord = () => <h1>Hello Word!!</h1>
+
+export default HelloWord
---
+++ React/SimpleProyect/src/index.js
@@ -0,0 +1,5 @@
+import React from 'React'
+import ReactDOM from 'react-dom'
+import HelloWord from './components/HelloWord'
+
+ReactDOM.render(<HelloWord/>, document.getElementById('app'))
